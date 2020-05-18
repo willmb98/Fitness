c64d86aa62d3bb731cfdae9792a48df32793c10c
willmb98 <willb506@gmail.com>
2020-05-18 09:41:03 +0100

    Ver 1.3

---
+++ app/src/main/java/com/example/fitness/ui/ViewWorkoutFragment.kt
@@ -0,0 +1,20 @@
+package com.example.fitness.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.fitness.*
+
+
+class ViewWorkoutFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_view_workout, container, false)
+    }
+
+}
