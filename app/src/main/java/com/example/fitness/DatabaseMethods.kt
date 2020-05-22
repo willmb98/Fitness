27d902e27c37c10f0947511ad287571a18fb6c80
Alex <s5112102@bournemouth.ac.uk>
2020-05-22 01:55:30 +0100

    added register

--- app/src/main/java/com/example/fitness/DatabaseMethods.kt
+++ app/src/main/java/com/example/fitness/DatabaseMethods.kt
@@ -1,18 +1,13 @@
 package com.example.fitness
 
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.json.JSONArray
 import org.json.JSONObject
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 
 object DatabaseMethods {
-    fun checkLogin(userName: String, password: String):JSONObject?{
-        val json = extractJson("https://demo.agwork.co.uk/db/login.php?username=$userName&password=$password")
+    fun checkLogin(username: String, password: String):JSONObject?{
+        val json = extractJson("https://demo.agwork.co.uk/db/login.php?username=$username&password=$password")
         return if (json.length() == 1) {
             JSONObject(json[0].toString())
         } else {
@@ -20,13 +15,13 @@ object DatabaseMethods {
         }
     }
 
-    fun register(userId: String, password: String) {
+    fun register(username: String, password: String, email: String, firstName: String, lastName: String) {
+        Jsoup.connect("https://demo.agwork.co.uk/db/register.php?username=$username&password=$password&email=$email&firstname=$firstName&secondname=$lastName").get()
 
     }
 
     fun updateCoins(newCoins: Int, userId:Int) {
         Jsoup.connect("https://demo.agwork.co.uk/db/coinupdate.php?newcoins=$newCoins&id=$userId").get()
-        Log.i("url", "https://demo.agwork.co.uk/db/coinupdate.php?newcoins=$newCoins&id=$userId")
     }
 
     fun extractJson(url: String):JSONArray {
